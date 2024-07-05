30024434384b0ff462f074eecd8580c448e136e1
Taylor Ng <taylormng95@gmail.com>
2024-07-05 15:47:08 -0400

    creates Opponent class

---
+++ lib/models/opponent.py
@@ -0,0 +1,15 @@
+class Opponent:
+    def __init__(self, name, id=None):
+        self.name = name
+        self.id = id
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise Exception
