9853d4003a916bf46c9bb5d878283d2f92d96bab
Taylor Ng <taylormng95@gmail.com>
2024-07-06 10:30:20 -0400

    completes view_all_opponents method for Cli class

--- lib/cli.py
+++ lib/cli.py
@@ -1,6 +1,7 @@
 from helpers import exit_program, clear_screen
 from models.match import Match
-import ipdb
+from models.opponent import Opponent
+""" import ipdb """
 
 class Cli:
 
@@ -54,6 +55,7 @@ class Cli:
         print('ALL MATCHES:')
         print('')
         for match in all_matches:
+            # adjust spacing to ensure alignment depending on ID length
             spacing = '  '
             if 10 <= match.id < 100:
                 spacing = ' '
@@ -64,13 +66,31 @@ class Cli:
         self.options()
 
     def add_new_match(self):
+        """ print('ADDING NEW MATCH:')
+        print('')
+        print('In order to add a new match, you must know the opponent ID.')
+        print('Type "1" to proceed')
+        print('Type "2" to view all opponents')
+        print('Type "3" to create new opponent') """
         pass
 
     def delete_existing_match(self):
         pass
 
     def view_all_opponents(self):
-        pass
+        all_opponents = [value for key, value in Opponent.all.items()]
+        print('ALL OPPONENTS:')
+        print('')
+        for opponent in all_opponents:
+            # adjust spacing to ensure alignment depending on ID length
+            spacing = '  '
+            if 10 <= opponent.id < 100:
+                spacing = ' '
+            elif 100 <= opponent.id < 1000:
+                spacing = ''
+            print(f'ID: {opponent.id}{spacing}| {opponent.name}')
+            print('------')
+        self.options()
 
     def add_new_opponent(self):
         pass
